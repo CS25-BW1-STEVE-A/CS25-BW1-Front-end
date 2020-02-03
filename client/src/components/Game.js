e0c367ed9a3c89345263081bfd2ca1e0480533a6
devin-bielejec <devin.bieleje@gmail.com>
2020-02-03 14:07:05 -0500

    basic board
    Co-authored-by: Seth Lachman <lachmanseth17@gmail.com>

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,6 +1,23 @@
 import React from "react";
 import { createBoard, baseURL, axiosWithAuth } from "../utils/index";
 import axios from "axios";
+import styled from "styled-components";
+
+const Board = styled.div`
+  max-width: 500px;
+  margin: 20px auto;
+`;
+
+const Cell = styled.div`
+  width: 50px;
+  height: 50px;
+  border: 1px solid black;
+`;
+
+const Row = styled.div`
+  display: flex;
+`;
+
 export default function() {
   const [gameStarted, setGameStarted] = React.useState(false);
   const [board, setBoard] = React.useState(createBoard(10));
@@ -16,6 +33,17 @@ export default function() {
     <div>
       <h1>This is the best game</h1>
       <button onClick={handleClick}>Start Game</button>
+      <Board className="board">
+        {board.map((row, rowIdx) => {
+          return (
+            <Row className="row">
+              {board[rowIdx].map((col, colIdx) => {
+                return <Cell className="cell">{col}</Cell>;
+              })}
+            </Row>
+          );
+        })}
+      </Board>
     </div>
   );
 }
