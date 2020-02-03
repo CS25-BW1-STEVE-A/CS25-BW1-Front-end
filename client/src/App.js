6c8594b70f39bcf451733e1790985fda6bf84a96
devin-bielejec <devin.bieleje@gmail.com>
2020-02-03 13:13:56 -0500

    Initial File Structure
    Co-authored-by: Seth Lachman <lachmanseth17@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import PrivateRoute from "./components/PrivateRoute";
+import Game from "./components/Game";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/game">
+            <Game />
+          </PrivateRoute>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
