0a975e9ab4545b730878f9d4fd2feab2398e3149
Seth Lachman <lachmanseth17@gmail.com>
2020-02-04 19:14:48 -0500

    Updated styles for login and register form.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,38 +2,17 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 import { baseURL } from "../utils/index";
-import styled, { css } from "styled-components";
+import {
+  Flex,
+  Background,
+  Form,
+  SubmitButton,
+  StyledInput,
+  Warning,
+  StyledLink,
+  Text
+} from "./Styles";
 
-const LoginStyled = styled.form`
-  width: 25%;
-  margin: 15% auto;
-  display: flex;
-  flex-flow: column wrap;
-  align-items: center;
-`;
-
-const SubmitButton = styled.input`
-  margin: 30px 150px;
-  color: blue;
-  box-shadow: none;
-  border: 1px solid blue;
-  border-radius: 10px;
-  padding: 10px 10px;
-
-  ${props =>
-    !props.disabled &&
-    css`
-      background-color: blue;
-      color: white;
-      border: 1px solid white;
-    `}
-`;
-
-const StyledInput = styled.input`
-  width: 50%;
-  margin: 10px 0;
-  font-family: "Play", sans-serif;
-`;
 export default function Login() {
   const { register, handleSubmit, formState, errors } = useForm({
     mode: "onChange"
@@ -57,25 +36,34 @@ export default function Login() {
       .catch(err => console.log(err));
   };
 
-  console.log(errors);
   return (
-    <LoginStyled onSubmit={handleSubmit(onSubmit)}>
-      <h2>Login</h2>
-      <StyledInput
-        name="username"
-        placeholder="Username"
-        ref={register({ required: true })}
-      />
-      {errors.username && <span>This field is required</span>}
+    <Flex justifyContent="center">
+      <Background>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Login</h2>
+          <StyledInput
+            name="username"
+            placeholder="Username"
+            ref={register({ required: true })}
+          />
+          {errors.username && <Warning>This field is required</Warning>}
 
-      <StyledInput
-        name="password"
-        placeholder="Password"
-        ref={register({ required: true })}
-      />
-      {errors.username && <span>This field is required</span>}
+          <StyledInput
+            name="password"
+            placeholder="Password"
+            ref={register({ required: true })}
+          />
+          {errors.username && <Warning>This field is required</Warning>}
 
-      <SubmitButton type="submit" disabled={!formState.isValid} />
-    </LoginStyled>
+          <SubmitButton type="submit" disabled={!formState.isValid}>
+            Login
+          </SubmitButton>
+          <Text fontSize="0.7em">
+            Not registered?{" "}
+            <StyledLink to="/register">Create an account</StyledLink>
+          </Text>
+        </Form>
+      </Background>
+    </Flex>
   );
 }
