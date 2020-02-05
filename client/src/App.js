3fb5dbd7361259b96151b9941a7202a799c98b7b
Seth Lachman <lachmanseth17@gmail.com>
2020-02-04 21:38:25 -0500

    Added global styles with styled components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 import LandingPage from "./components/LandingPage";
 import PrivateRoute from "./components/PrivateRoute";
 import Game from "./components/Game";
 import Layout from "./components/Layout";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import "./App.css";
+
+const Global = createGlobalStyle` 
+  
+  body {
+    margin: 0;
+    background-color: #FBF9F7;
+    font-family: "Play", sans-serif;
+  }
+
+  @keyframes slidein {
+    from {
+      transform: translateX(0%);
+    }
+
+    to {
+      transform: translateX(100%);
+    }
+  }
+`;
 
 function App() {
   return (
     <>
       <Router>
+        <Global />
         <Layout>
           <Switch>
             <PrivateRoute path="/game">
